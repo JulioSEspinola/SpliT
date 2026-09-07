d27f69bd5d6c06cd66a7e26e0e2beb1bd93f8713
Julio S Espinola <52016566+JulioSEspinola@users.noreply.github.com>
2026-09-07 16:39:51 -0500

    added cors to connect backend to frontend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import users, groups, expenses
+from app.config import settings
 
 app = FastAPI(title="Splitwise Clone", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_allow_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(groups.router)
